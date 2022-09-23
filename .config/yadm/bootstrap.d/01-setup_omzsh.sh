96364473892c521dc29019afd56b60c16bfe2fc2
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2022-09-23 17:09:07 -0400

    feat: Added spaceship theme

--- .config/yadm/bootstrap.d/01-setup_omzsh.sh
+++ .config/yadm/bootstrap.d/01-setup_omzsh.sh
@@ -25,3 +25,8 @@ if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" ]; then
 fi
 
 
+if [ ! -d "$ZSH_CUSTOM/themes/spaceship-prompt" ]; then
+    echo "Installing spaceship-prompt"
+    git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+    ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+fi
