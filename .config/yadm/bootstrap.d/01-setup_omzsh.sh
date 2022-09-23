2dfe6ebe42d3e482e747438e022eebe7245fd802
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2022-09-23 16:49:48 -0400

    feat: Added a script to install extra plugins

--- .config/yadm/bootstrap.d/01-setup_omzsh.sh
+++ .config/yadm/bootstrap.d/01-setup_omzsh.sh
@@ -8,3 +8,20 @@ if [ ! -d "$HOME/.oh-my-zsh" ]; then
 else
     echo "oh-my-zsh already installed"
 fi
+
+if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ]; then
+    echo "Installing zsh-autosuggestions"
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+fi
+
+if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/evalcache" ]; then
+    echo "Installing evalcache"
+    git clone https://github.com/mroth/evalcache ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/evalcache
+fi
+
+if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" ]; then
+    echo "Installing zsh-syntax-highlighting"
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+fi
+
+
