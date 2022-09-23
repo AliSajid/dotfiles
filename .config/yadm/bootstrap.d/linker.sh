aa636558bc52639e3ece3ecfcb50d8e22db6f0dd
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2022-09-23 14:29:37 -0400

    feat: Added a linker script that only echoes stuff

---
+++ .config/yadm/bootstrap.d/linker.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -euo pipefail
+
+echo "$(pwd)"
+
+files=$(find . -type f -not -path '*/.git/*' | grep -v README | grep -v LICENSE)
+
+for f in $files; do
+    basedir=$(dirname $f)
+    filename=$(basename $f)
+    target="$MYHOME/$basedir/$filename"
+
+    if [ ! -d $basedir ]; then
+        echo "$basedir does not exist, creating"
+        mkdir -p $basedir
+    fi
+
+    if [ -f "$target" ] ; then
+        echo "$target already exists, moving to $target.bak"
+        echo mv "$target" "$target.bak"
+    else
+        echo "$target does not exist, linking"
+        echo ln -s "$f" "$target"
+    fi
+done
