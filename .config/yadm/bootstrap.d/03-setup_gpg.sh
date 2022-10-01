22771286e6f2f7d408b9aa7527127a545cf5d536
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2022-09-30 23:38:12 -0400

    feat: Added a script to import the GPG keys

---
+++ .config/yadm/bootstrap.d/03-setup_gpg.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -euo pipefail
+
+echo "Setting up the GPG Keys"
+
+if [ ! -z $GPG_KEY  ]]; then
+    echo "GPG Key is set, importing"
+    gpg --verbose --batch --import <(echo $GPG_KEY|base64 -d)
+    echo 'pinentry-mode loopback' >> ~/.gnupg/gpg.conf
+    echo 'allow-loopback-pinentry' >> ~/.gnupg/gpg-agent.conf
+fi
